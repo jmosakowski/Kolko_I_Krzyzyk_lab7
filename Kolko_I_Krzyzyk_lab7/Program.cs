abe92bc35d13811200d5204d0484d5ef1775b5ef
Jacek Mosakowski <Jacek Mosakowski@DESKTOP-RIS73BT>
2020-03-29 11:39:41 +0200

    First version, added player names and game board

--- Kolko_I_Krzyzyk_lab7/Program.cs
+++ Kolko_I_Krzyzyk_lab7/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kolko_I_Krzyzyk_lab7
 {
@@ -10,6 +6,27 @@ namespace Kolko_I_Krzyzyk_lab7
     {
         static void Main(string[] args)
         {
+            string imieGraczaA = "";
+            string imieGraczaB = "";
+            char znakGraczaA = 'x';
+            char znakGraczaB = 'o';
+
+            int[] tablica1D = new int[4] { 5, 10, 15, 100 };
+
+            char[,] plansza = new char[3, 3] {
+                { '1', '2', '3' },
+                { '4', '5', '6' },
+                { '7', '8', '9' }
+            };
+
+            Console.Write("Wpisz imie gracza A: ");
+            imieGraczaA = Console.ReadLine();
+            Console.Write("Wpisz imie gracza B: ");
+            imieGraczaB = Console.ReadLine();
+
+            // ...
+
+            Console.ReadKey();  // nacisnij dowolny klawisz zeby zakonczyc
         }
     }
 }
